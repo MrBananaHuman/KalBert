3c1ed41c8c3e355fec200eb5e4357ea1d4e3c804
MrBananaHuman <34882690+MrBananaHuman@users.noreply.github.com>
2019-10-28 15:27:00 +0900

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -26,7 +26,7 @@ import re
 import numpy as np
 import six
 import tensorflow as tf
-
+import bert_utils
 
 class BertConfig(object):
   """Configuration for `BertModel`."""
@@ -170,11 +170,12 @@ class BertModel(object):
 
     with tf.variable_scope(scope, default_name="bert"):
       with tf.variable_scope("embeddings"):
-        # Perform embedding lookup on the word ids.
-        (self.embedding_output, self.embedding_table, self.embedding_temp_table) = embedding_lookup(
+        # Perform embedding lookup on the word ids, but use stype of factorized embedding parameterization from albert. add by brightmart, 2019-09-28
+        (self.embedding_output, self.embedding_table,self.embedding_table_2) = embedding_lookup_factorized(
             input_ids=input_ids,
             vocab_size=config.vocab_size,
-            embedding_size=config.hidden_size,
+            hidden_size=config.hidden_size,
+            embedding_size=config.embedding_size,
             initializer_range=config.initializer_range,
             word_embedding_name="word_embeddings",
             use_one_hot_embeddings=use_one_hot_embeddings)
@@ -202,20 +203,39 @@ class BertModel(object):
 
         # Run the stacked transformer.
         # `sequence_output` shape = [batch_size, seq_length, hidden_size].
-        self.all_encoder_layers = transformer_model(
-            input_tensor=self.embedding_output,
-            attention_mask=attention_mask,
-            hidden_size=config.hidden_size,
-            num_hidden_layers=config.num_hidden_layers,
-            num_attention_heads=config.num_attention_heads,
-            intermediate_size=config.intermediate_size,
-            intermediate_act_fn=get_activation(config.hidden_act),
-            hidden_dropout_prob=config.hidden_dropout_prob,
-            attention_probs_dropout_prob=config.attention_probs_dropout_prob,
-            initializer_range=config.initializer_range,
-            do_return_all_layers=True)
+        ln_type=config.ln_type
+        print("ln_type:",ln_type)
+        if ln_type=='postln' or ln_type is None: # currently, base or large of albert used post-LN structure
+            print("old structure of transformer.use: transformer_model,which use post-LN")
+            self.all_encoder_layers = transformer_model(
+                input_tensor=self.embedding_output,
+                attention_mask=attention_mask,
+                hidden_size=config.hidden_size,
+                num_hidden_layers=config.num_hidden_layers,
+                num_attention_heads=config.num_attention_heads,
+                intermediate_size=config.intermediate_size,
+                intermediate_act_fn=get_activation(config.hidden_act),
+                hidden_dropout_prob=config.hidden_dropout_prob,
+                attention_probs_dropout_prob=config.attention_probs_dropout_prob,
+                initializer_range=config.initializer_range,
+                do_return_all_layers=True)
+        else: # xlarge or xxlarge of albert, used pre-LN structure
+            print("new structure of transformer.use: prelln_transformer_model,which use pre-LN")
+            self.all_encoder_layers = prelln_transformer_model( # change by brightmart, 4th, oct, 2019. pre-Layer Normalization can converge fast and better. check paper: ON LAYER NORMALIZATION IN THE TRANSFORMER ARCHITECTURE
+                input_tensor=self.embedding_output,
+                attention_mask=attention_mask,
+                hidden_size=config.hidden_size,
+                num_hidden_layers=config.num_hidden_layers,
+                num_attention_heads=config.num_attention_heads,
+                intermediate_size=config.intermediate_size,
+                intermediate_act_fn=get_activation(config.hidden_act),
+                hidden_dropout_prob=config.hidden_dropout_prob,
+                attention_probs_dropout_prob=config.attention_probs_dropout_prob,
+                initializer_range=config.initializer_range,
+                do_return_all_layers=True,
+                shared_type='all') #  do_return_all_layers=True
 
-      self.sequence_output = self.all_encoder_layers[-1]
+      self.sequence_output = self.all_encoder_layers[-1] # [batch_size, seq_length, hidden_size]
       # The "pooler" converts the encoded sequence tensor of shape
       # [batch_size, seq_length, hidden_size] to a tensor of shape
       # [batch_size, hidden_size]. This is necessary for segment-level
@@ -260,6 +280,8 @@ class BertModel(object):
   def get_embedding_table(self):
     return self.embedding_table
 
+  def get_embedding_table_2(self):
+    return self.embedding_table_2
 
 def gelu(x):
   """Gaussian Error Linear Unit.
@@ -404,42 +426,82 @@ def embedding_lookup(input_ids,
   # If the input is a 2D tensor of shape [batch_size, seq_length], we
   # reshape to [batch_size, seq_length, 1].
   if input_ids.shape.ndims == 2:
-    input_ids = tf.expand_dims(input_ids, axis=[-1])
+    input_ids = tf.expand_dims(input_ids, axis=[-1]) # shape of input_ids is:[ batch_size, seq_length, 1]
 
-  embedding_table = tf.get_variable(
+  embedding_table = tf.get_variable( # [vocab_size, embedding_size]
       name=word_embedding_name,
       shape=[vocab_size, embedding_size],
       initializer=create_initializer(initializer_range))
 
-  flat_input_ids = tf.reshape(input_ids, [-1])
+  flat_input_ids = tf.reshape(input_ids, [-1]) # one rank. shape as (batch_size * sequence_length,)
   if use_one_hot_embeddings:
-    one_hot_input_ids = tf.one_hot(flat_input_ids, depth=vocab_size)
-    #modified
-    embedding_temp = tf.matmul(one_hot_input_ids, embedding_table)
+    one_hot_input_ids = tf.one_hot(flat_input_ids, depth=vocab_size) # one_hot_input_ids=[batch_size * sequence_length,vocab_size]
+    output = tf.matmul(one_hot_input_ids, embedding_table) # output=[batch_size * sequence_length,embedding_size]
   else:
-    #modified
-    embedding_temp = tf.gather(embedding_table, flat_input_ids)
+    output = tf.gather(embedding_table, flat_input_ids) # [vocab_size, embedding_size]*[batch_size * sequence_length,]--->[batch_size * sequence_length,embedding_size]
 
-  #modified
-  embedding_temp_table = tf.get_variable(
-      name=word_embedding_name+"_temp",
-      shape=[embedding_size, hidden_size],
-      ititializer=create_initialize(initializer_range))
+  input_shape = get_shape_list(input_ids) # input_shape=[ batch_size, seq_length, 1]
 
-  #modified
-  output = tf.matmul(embedding_temp, embedding_temp_table)
+  output = tf.reshape(output,input_shape[0:-1] + [input_shape[-1] * embedding_size]) # output=[batch_size,sequence_length,embedding_size]
+  return (output, embedding_table)
 
-  input_shape = get_shape_list(input_ids)
+def embedding_lookup_factorized(input_ids, # Factorized embedding parameterization provide by albert
+                     vocab_size,
+                     hidden_size,
+                     embedding_size=128,
+                     initializer_range=0.02,
+                     word_embedding_name="word_embeddings",
+                     use_one_hot_embeddings=False):
+    """Looks up words embeddings for id tensor, but in a factorized style followed by albert. it is used to reduce much percentage of parameters previous exists.
+       Check "Factorized embedding parameterization" session in the paper.
 
-  #???
-  batch_size, sequene_length, _ = input_shape
-  
-  output = tf.reshape(output,(batch_size,sequene_length,hidden_size)
-  #output = tf.reshape(output,
-  #                    input_shape[0:-1] + [input_shape[-1] * embedding_size])
+     Args:
+       input_ids: int32 Tensor of shape [batch_size, seq_length] containing word
+         ids.
+       vocab_size: int. Size of the embedding vocabulary.
+       embedding_size: int. Width of the word embeddings.
+       initializer_range: float. Embedding initialization range.
+       word_embedding_name: string. Name of the embedding table.
+       use_one_hot_embeddings: bool. If True, use one-hot method for word
+         embeddings. If False, use `tf.gather()`.
 
-  #return (output, embedding_table)
-  return (output, embedding_table, embedding_temp_table)
+     Returns:
+       float Tensor of shape [batch_size, seq_length, embedding_size].
+     """
+    # This function assumes that the input is of shape [batch_size, seq_length,
+    # num_inputs].
+    #
+    # If the input is a 2D tensor of shape [batch_size, seq_length], we
+    # reshape to [batch_size, seq_length, 1].
+
+    # 1.first project one-hot vectors into a lower dimensional embedding space of size E
+    print("embedding_lookup_factorized. factorized embedding parameterization is used.")
+    if input_ids.shape.ndims == 2:
+        input_ids = tf.expand_dims(input_ids, axis=[-1])  # shape of input_ids is:[ batch_size, seq_length, 1]
+
+    embedding_table = tf.get_variable(  # [vocab_size, embedding_size]
+        name=word_embedding_name,
+        shape=[vocab_size, embedding_size],
+        initializer=create_initializer(initializer_range))
+
+    flat_input_ids = tf.reshape(input_ids, [-1])  # one rank. shape as (batch_size * sequence_length,)
+    if use_one_hot_embeddings:
+        one_hot_input_ids = tf.one_hot(flat_input_ids,depth=vocab_size)  # one_hot_input_ids=[batch_size * sequence_length,vocab_size]
+        output_middle = tf.matmul(one_hot_input_ids, embedding_table)  # output=[batch_size * sequence_length,embedding_size]
+    else:
+        output_middle = tf.gather(embedding_table,flat_input_ids)  # [vocab_size, embedding_size]*[batch_size * sequence_length,]--->[batch_size * sequence_length,embedding_size]
+
+    # 2. project vector(output_middle) to the hidden space
+    project_variable = tf.get_variable(  # [embedding_size, hidden_size]
+        name=word_embedding_name+"_2",
+        shape=[embedding_size, hidden_size],
+        initializer=create_initializer(initializer_range))
+    output = tf.matmul(output_middle, project_variable) # ([batch_size * sequence_length, embedding_size] * [embedding_size, hidden_size])--->[batch_size * sequence_length, hidden_size]
+    # reshape back to 3 rank
+    input_shape = get_shape_list(input_ids)  # input_shape=[ batch_size, seq_length, 1]
+    batch_size, sequene_length, _=input_shape
+    output = tf.reshape(output, (batch_size,sequene_length,hidden_size))  # output=[batch_size, sequence_length, hidden_size]
+    return (output, embedding_table, project_variable)
 
 
 def embedding_postprocessor(input_tensor,
@@ -778,7 +840,8 @@ def transformer_model(input_tensor,
                       hidden_dropout_prob=0.1,
                       attention_probs_dropout_prob=0.1,
                       initializer_range=0.02,
-                      do_return_all_layers=False):
+                      do_return_all_layers=False,
+                      share_parameter_across_layers=True):
   """Multi-headed, multi-layer Transformer from "Attention is All You Need".
 
   This is almost an exact implementation of the original Transformer encoder.
@@ -841,8 +904,13 @@ def transformer_model(input_tensor,
 
   all_layer_outputs = []
   for layer_idx in range(num_hidden_layers):
-    #sharing parameters
-    with tf.variable_scope("shared_layer"):
+    if share_parameter_across_layers:
+        name_variable_scope="layer_shared"
+    else:
+        name_variable_scope="layer_%d" % layer_idx
+    # share all parameters across layers. add by brightmart, 2019-09-28. previous it is like this: "layer_%d" % layer_idx
+    with tf.variable_scope(name_variable_scope, reuse=True if (share_parameter_across_layers and layer_idx>0) else False):
+
       layer_input = prev_output
 
       with tf.variable_scope("attention"):
@@ -1002,3 +1070,195 @@ def assert_rank(tensor, expected_rank, name=None):
         "For the tensor `%s` in scope `%s`, the actual rank "
         "`%d` (shape = %s) is not equal to the expected rank `%s`" %
         (name, scope_name, actual_rank, str(tensor.shape), str(expected_rank)))
+
+def prelln_transformer_model(input_tensor,
+						attention_mask=None,
+						hidden_size=768,
+						num_hidden_layers=12,
+						num_attention_heads=12,
+						intermediate_size=3072,
+						intermediate_act_fn=gelu,
+						hidden_dropout_prob=0.1,
+						attention_probs_dropout_prob=0.1,
+						initializer_range=0.02,
+						do_return_all_layers=False,
+						shared_type='all', # None,
+						adapter_fn=None):
+	"""Multi-headed, multi-layer Transformer from "Attention is All You Need".
+
+	This is almost an exact implementation of the original Transformer encoder.
+
+	See the original paper:
+	https://arxiv.org/abs/1706.03762
+
+	Also see:
+	https://github.com/tensorflow/tensor2tensor/blob/master/tensor2tensor/models/transformer.py
+
+	Args:
+		input_tensor: float Tensor of shape [batch_size, seq_length, hidden_size].
+		attention_mask: (optional) int32 Tensor of shape [batch_size, seq_length,
+			seq_length], with 1 for positions that can be attended to and 0 in
+			positions that should not be.
+		hidden_size: int. Hidden size of the Transformer.
+		num_hidden_layers: int. Number of layers (blocks) in the Transformer.
+		num_attention_heads: int. Number of attention heads in the Transformer.
+		intermediate_size: int. The size of the "intermediate" (a.k.a., feed
+			forward) layer.
+		intermediate_act_fn: function. The non-linear activation function to apply
+			to the output of the intermediate/feed-forward layer.
+		hidden_dropout_prob: float. Dropout probability for the hidden layers.
+		attention_probs_dropout_prob: float. Dropout probability of the attention
+			probabilities.
+		initializer_range: float. Range of the initializer (stddev of truncated
+			normal).
+		do_return_all_layers: Whether to also return all layers or just the final
+			layer.
+
+	Returns:
+		float Tensor of shape [batch_size, seq_length, hidden_size], the final
+		hidden layer of the Transformer.
+
+	Raises:
+		ValueError: A Tensor shape or parameter is invalid.
+	"""
+	if hidden_size % num_attention_heads != 0:
+		raise ValueError(
+				"The hidden size (%d) is not a multiple of the number of attention "
+				"heads (%d)" % (hidden_size, num_attention_heads))
+
+	attention_head_size = int(hidden_size / num_attention_heads)
+
+	input_shape = bert_utils.get_shape_list(input_tensor, expected_rank=3)
+	batch_size = input_shape[0]
+	seq_length = input_shape[1]
+	input_width = input_shape[2]
+
+	# The Transformer performs sum residuals on all layers so the input needs
+	# to be the same as the hidden size.
+	if input_width != hidden_size:
+		raise ValueError("The width of the input tensor (%d) != hidden size (%d)" %
+										 (input_width, hidden_size))
+
+	# We keep the representation as a 2D tensor to avoid re-shaping it back and
+	# forth from a 3D tensor to a 2D tensor. Re-shapes are normally free on
+	# the GPU/CPU but may not be free on the TPU, so we want to minimize them to
+	# help the optimizer.
+	prev_output = bert_utils.reshape_to_matrix(input_tensor)
+
+	all_layer_outputs = []
+
+	def layer_scope(idx, shared_type):
+		if shared_type == 'all':
+			tmp = {
+				"layer":"layer_shared",
+				'attention':'attention',
+				'intermediate':'intermediate',
+				'output':'output'
+			}
+		elif shared_type == 'attention':
+			tmp = {
+				"layer":"layer_shared",
+				'attention':'attention',
+				'intermediate':'intermediate_{}'.format(idx),
+				'output':'output_{}'.format(idx)
+			}
+		elif shared_type == 'ffn':
+			tmp = {
+				"layer":"layer_shared",
+				'attention':'attention_{}'.format(idx),
+				'intermediate':'intermediate',
+				'output':'output'
+			}
+		else:
+			tmp = {
+				"layer":"layer_{}".format(idx),
+				'attention':'attention',
+				'intermediate':'intermediate',
+				'output':'output'
+			}
+
+		return tmp
+
+	all_layer_outputs = []
+
+	for layer_idx in range(num_hidden_layers):
+
+		idx_scope = layer_scope(layer_idx, shared_type)
+
+		with tf.variable_scope(idx_scope['layer'], reuse=tf.AUTO_REUSE):
+			layer_input = prev_output
+
+			with tf.variable_scope(idx_scope['attention'], reuse=tf.AUTO_REUSE):
+				attention_heads = []
+
+				with tf.variable_scope("output", reuse=tf.AUTO_REUSE):
+					layer_input_pre = layer_norm(layer_input)
+
+				with tf.variable_scope("self"):
+					attention_head = attention_layer(
+							from_tensor=layer_input_pre,
+							to_tensor=layer_input_pre,
+							attention_mask=attention_mask,
+							num_attention_heads=num_attention_heads,
+							size_per_head=attention_head_size,
+							attention_probs_dropout_prob=attention_probs_dropout_prob,
+							initializer_range=initializer_range,
+							do_return_2d_tensor=True,
+							batch_size=batch_size,
+							from_seq_length=seq_length,
+							to_seq_length=seq_length)
+					attention_heads.append(attention_head)
+
+				attention_output = None
+				if len(attention_heads) == 1:
+					attention_output = attention_heads[0]
+				else:
+					# In the case where we have other sequences, we just concatenate
+					# them to the self-attention head before the projection.
+					attention_output = tf.concat(attention_heads, axis=-1)
+
+				# Run a linear projection of `hidden_size` then add a residual
+				# with `layer_input`.
+				with tf.variable_scope("output", reuse=tf.AUTO_REUSE):
+					attention_output = tf.layers.dense(
+							attention_output,
+							hidden_size,
+							kernel_initializer=create_initializer(initializer_range))
+					attention_output = dropout(attention_output, hidden_dropout_prob)
+
+					# attention_output = layer_norm(attention_output + layer_input)
+					attention_output = attention_output + layer_input
+
+			with tf.variable_scope(idx_scope['output'], reuse=tf.AUTO_REUSE):
+				attention_output_pre = layer_norm(attention_output)
+
+			# The activation is only applied to the "intermediate" hidden layer.
+			with tf.variable_scope(idx_scope['intermediate'], reuse=tf.AUTO_REUSE):
+				intermediate_output = tf.layers.dense(
+						attention_output_pre,
+						intermediate_size,
+						activation=intermediate_act_fn,
+						kernel_initializer=create_initializer(initializer_range))
+
+			# Down-project back to `hidden_size` then add the residual.
+			with tf.variable_scope(idx_scope['output'], reuse=tf.AUTO_REUSE):
+				layer_output = tf.layers.dense(
+						intermediate_output,
+						hidden_size,
+						kernel_initializer=create_initializer(initializer_range))
+				layer_output = dropout(layer_output, hidden_dropout_prob)
+
+				# layer_output = layer_norm(layer_output + attention_output)
+				layer_output = layer_output + attention_output
+				prev_output = layer_output
+				all_layer_outputs.append(layer_output)
+
+	if do_return_all_layers:
+		final_outputs = []
+		for layer_output in all_layer_outputs:
+			final_output = bert_utils.reshape_from_matrix(layer_output, input_shape)
+			final_outputs.append(final_output)
+		return final_outputs
+	else:
+		final_output = bert_utils.reshape_from_matrix(prev_output, input_shape)
+		return final_output
